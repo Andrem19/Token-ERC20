969daa388a5f47f86b360576560def137d42e77f
Andrew <7255591@gmail.com>
2021-07-19 13:48:49 +0100

    add transfer and balanceOf in Token.sol

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,12 +2,29 @@
 pragma solidity ^0.5.0;
 
 contract Token {
+
   string public name = "My Name";
   string public symbol = "DTOK";
   uint256 public decimals = 18;
   uint256 public totalSupply;
 
+  //Track balances
+  mapping(address => uint256) public balanceOf;
+
+  event Transfer(address indexed from, address indexed to, uint256 _value);
+
   constructor() public {
   	totalSupply = 1000000 * (10**decimals);
+    balanceOf[msg.sender] = totalSupply;
+  }
+  //Send tokens
+  function transfer(address _to, uint256 _value) public returns(bool success) {
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
+
+
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,17 +1,17 @@
 //ERC-20 Token Standard
-
+import { tokens } from './helpers'
 const Token = artifacts.require('./Token')
 require('chai')
 	.use(require('chai-as-promised'))
 	.should()
 
-contract('Token', (acc) => {
+contract('Token', (accounts) => {
 	let token
 
 	const myName = 'My Name'
 	const symb = 'DTOK'
 	const decim = '18'
-	const totalSup = '1000000000000000000000000'
+	const totalSup = tokens(1000000).toString()//'1000000000000000000000000'
 
 	beforeEach(async () => {
 		token = await Token.new()
@@ -36,5 +36,37 @@ contract('Token', (acc) => {
 			const res = await token.totalSupply()
 			res.toString().should.equal(totalSup)
 		})
+
+		it('assigns the total supply to the deployer', async () => {
+			const res = await token.balanceOf(accounts[0])
+			res.toString().should.equal(totalSup)
+		})
+	})
+
+	describe('deployment', () => {
+	    it('transfers token balances', async () => {
+	        let balanceOf
+	        let deployer = accounts[0]
+	        let receiver = accounts[1]
+
+	        balanceOf = await token.balanceOf(deployer)
+	        console.log("deployer balance before transfer", balanceOf.toString())
+
+	        balanceOf = await token.balanceOf(receiver)
+	        console.log("receiver balance before transfer", balanceOf.toString())
+
+            //Transfer
+            await token.transfer(receiver, tokens(100), { from: deployer})//function metadata
+            //After transfer
+            balanceOf = await token.balanceOf(deployer)
+	        console.log("deployer balance after transfer", balanceOf.toString())
+	        balanceOf.toString().should.equal(tokens(999900).toString())
+
+	        balanceOf = await token.balanceOf(receiver)
+	        console.log("receiver balance after transfer", balanceOf.toString())
+	        balanceOf.toString().should.equal(tokens(100).toString())
+
+
+	    })
 	})
 })
